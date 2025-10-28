df931b641ef32c1f17abadc182b0648f058c4be0
Leonardo Rossi de Oliveira <83621860+leorossiio@users.noreply.github.com>
2025-10-28 20:32:35 -0300

    Update AlunoService.java

--- src/main/java/com/example/Pratica4/service/AlunoService.java
+++ src/main/java/com/example/Pratica4/service/AlunoService.java
@@ -1,17 +1,16 @@
 package com.example.Pratica4.service;
 
 import com.example.Pratica4.domain.Aluno;
+import com.example.Pratica4.domain.StatusAluno;
 
 public class AlunoService {
-
-    private static final int CURSOS_PREMIUM = 12;
-
+    
     public void registrarParticipacaoNoForum(Aluno aluno, String mensagem) {
-        aluno.setCursosConcluidos(aluno.getCursosConcluidos() + 1);
-        atualizarPlanoSeElegivel(aluno);
-    }
-
-    public void atualizarPlanoSeElegivel(Aluno aluno) {
-        aluno.setPremium(aluno.getCursosConcluidos() >= CURSOS_PREMIUM);
+        
+        StatusAluno statusAtual = aluno.getStatus();
+        
+        StatusAluno proximoStatus = statusAtual.adicionarCurso();
+        
+        aluno.setStatus(proximoStatus);
     }
 }
